1a46f473d7703f8a499d3494961c59dea90d2019
aannoo <ustbians.ai.and.ml@gmail.com>
2024-02-02 01:46:19 +0500

    class based views: ListView and DetailView added

--- ustbian/blog/urls.py
+++ ustbian/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from . views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='detail'), # if the records for id 1,2 are deleted then the pk will start from 3 for new entries
     path('about/', views.about, name='about'),
 ]
--- ustbian/blog/views.py
+++ ustbian/blog/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . models import Post
 
+# Function based view
+'''
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+'''
+
+# Class based view
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    # ordering = ['-created_at']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
 
 def about(request):
     return render(request, 'blog/about.html')
